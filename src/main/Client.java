892d1a43ec7a1e1894b4df45f6833007e2c59e31
Catherine <catherine@Chuns-MacBook-Pro.local>
2013-04-29 22:48:32 -0400

    Updated Client/User methods

--- src/main/Client.java
+++ src/main/Client.java
@@ -4,7 +4,29 @@ package main;
  * GUI chat client runner.
  */
 public class Client {
-
+    
+    //The Server this Client is connected to
+    private final Server myServer;
+    //The username of this Client
+    private final String username;
+    
+    public Client(Server s, String username)
+    {
+        this.myServer = s;
+        this.username = username;  
+    }
+    public void joinConversation(int id)
+    {
+        //Join the Conversation with ID id.  If this is already a member, do nothing.
+    }
+    public void leaveConversation(int id)
+    {
+        //Leave the Conversation with ID id.  If this is not a member, do nothing.  
+    }
+    public boolean isMemberOf(int id)
+    {
+        //See if this Client is a member of the Conversation with ID id.
+    }
     /**
      * Start a GUI chat client.
      */
