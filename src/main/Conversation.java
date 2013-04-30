cedd082901d7ba76c80f687e63d75286f7cdeed8
Catherine <catherine@Chuns-MacBook-Pro.local>
2013-04-29 22:28:56 -0400

    Added some methods

--- src/main/Conversation.java
+++ src/main/Conversation.java
@@ -9,6 +9,7 @@ public class Conversation {
     //The List of all current participants in the Conversation.  
     //Invariant: There must be at least one Client in a Conversation's users List.  
     private List<Client> users = new ArrayList<Client>();
+    private List<Message> messages = new ArrayList<Message>();
     
     public Conversation(int id, Client starter)
     {
@@ -24,6 +25,19 @@ public class Conversation {
     {
         //if this.users.has(user) { users.remove(user) }
         //if this.users.size() == 0 { //delete Conversation }
+    }
+    public void addMessage(Message m)
+    {
+        //check if message's author is in this conversation; if so, add to this.messages
+    }
+    public void getMessagesFrom(Date d)
+    {
+        //get all Messages starting from this Date
+    }
+    public int getID()
+    {
+        //Return this Conversation's ID
+        return this.id;
     }
 
 }
